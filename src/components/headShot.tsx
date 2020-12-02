658b56bf80c85cb2bd664639e43a5db829571ada
Dean McHugh <dean.mchugh1988@gmail.com>
2020-12-02 20:25:19 +1000

    ADD: headshot image with styling

---
+++ src/components/headShot.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "headShot.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+    return <div>Picture not found</div>
+  }
+
+  return (
+    <Img 
+      fluid={data.placeholderImage.childImageSharp.fluid} 
+      imgStyle={{
+        borderRadius: `50%`,
+      }}
+    />
+  )
+}
+
+export default Image
